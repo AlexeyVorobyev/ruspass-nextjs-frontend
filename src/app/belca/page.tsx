0881a53da7147163a576026e6961e5f1fdc0f7ff
hochuspat <persikiafuk@gmail.com>
2024-03-31 08:48:35 +0300

    описание

---
+++ src/app/belca/page.tsx
@@ -0,0 +1,6 @@
+import IPhone1314 from '@/pages/main copy/belca-page.component'
+
+
+export default function IPhone() {
+    return <IPhone1314/>
+}
---
+++ src/app/plan/page.tsx
@@ -0,0 +1,6 @@
+import Mob from '@/pages/plan/plan-page.component'
+
+
+export default function Plan() {
+    return <Mob/>
+}
